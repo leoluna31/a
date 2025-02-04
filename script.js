c01535519a4b3d433f21c8d7c6faf644ebf486fd
leoluna31 <l24100526@nlaredo.tecnm.mx>
2025-02-04 16:45:55 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Estas segura?",
+    "Muy segura??",
+    "de verdad estas segura?",
+    "Pookie please...",
+    "Just think about it!",
+    "Si dices que no, estare muy triste...",
+    "Voy a estar muy triste...",
+    "Voy a estar muy muy triste...",
+    "Esta bien, ya no te voy a preguntar...",
+    "no te creas, ya di que si por favor ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
